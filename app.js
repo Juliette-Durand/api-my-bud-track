8e5e9824d20689d725e25d85666c5c0fb8ef535e
Juliette Durand <juliette.durand.pro4@gmail.com>
2025-12-19 11:27:55 +0100

    feat(app.js): add routes for users

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 
 const roleAppRoutes = require('./routes/roleAppRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
 
 // Middleware pour traiter les données JSON
 app.use(express.json());
 
 // Utilisation du routeur pour les rôles de l'application sous la route /role-app
 app.use('/role-app', roleAppRoutes);
+// Utilisation du routeur pour les utilisateurs sous la route /users
+app.use('/users', userRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
