09c21ef6c4223d0edaf5a0661e8595612b7a6aaf
Juliette Durand <j.durand@3magroup.com>
2026-01-15 13:28:24 +0100

    feat(repository): add accountRepository.js to manage database interactions for accounts

---
+++ repositories/accountRepository.js
@@ -0,0 +1,121 @@
+const prisma = require('../config/prisma');
+
+/**
+ * Récupération de tous les comptes
+ * @returns {Array} tableau des comptes
+ */
+async function getAllAccounts() {
+
+    const accounts = await prisma.account.findMany({
+        where: {
+            isActive: true
+        },
+        include: {
+            typeAccount: {
+                select: {
+                    id: true,
+                    label: true
+                }
+            }
+        }
+    });
+    
+    return accounts;
+}
+
+/**
+ * Récupération d'un compte
+ * @param {Number} id identifiant du compte
+ * @returns {Object} tableau du compte correspondant à l'id
+ */
+async function getAccountById(id) {
+
+    const account = await prisma.account.findFirst({
+        where: {
+            id: id,
+            isActive: true
+        },
+        include: {
+            typeAccount: {
+                select: {
+                    id: true,
+                    label: true
+                }
+            }
+        }
+    });
+    
+    return account;
+}
+
+/**
+ * Ajout d'un compte en base de données
+ * @param {Object} account objet contenant les infos du compte
+ * @returns {Object} contenant les infos du compte inséré
+ */
+async function addAccount(account) {
+    
+    const { label, balance, typeAccountId } = account;
+
+    const result = await prisma.account.create({
+        data: {
+            label: label,
+            balance: balance,
+            typeAccount: {
+                connect: { id: typeAccountId }
+            }
+        }
+    });
+
+    return result;
+}
+
+/**
+ * Modifier les informations d'un compte via son id
+ * @param {Number} id identifiant du compte
+ * @param {Object} account objet contenant les nouvelles infos du compte
+ * @returns {Object} contenant les infos du compte modifié
+ */
+async function updateAccount(id, account) {
+    
+    const { label, balance, typeAccountId } = account;
+
+    const result = await prisma.account.update({
+        where: {
+            id: id
+        },
+        data: {
+            label: label,
+            balance: balance,
+            typeAccount: {
+                connect: { id: typeAccountId }
+            }
+        }
+    });
+
+    return result;
+}
+
+/**
+ * Supprimer un compte via son id
+ * @param {Number} id identifiant du compte
+ * @returns {Object} contenant les infos du compte supprimé
+ */
+async function deleteAccount(id) {
+
+    const result = await prisma.account.delete({
+        where: {
+            id: id
+        }
+    });    
+
+    return result;
+}
+
+module.exports = {
+    getAllAccounts,
+    getAccountById,
+    addAccount,
+    updateAccount,
+    deleteAccount
+}
