83102c4ec81243dc966cad365b636f7582e9cbd3
Juliette Durand <j.durand@3magroup.com>
2026-01-15 13:31:56 +0100

    feat(app.js): add routes for accounts

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const roleAppRoutes = require('./routes/roleAppRoutes.js');
 const userRoutes = require('./routes/userRoutes.js');
 const typeAccountRoutes = require('./routes/typeAccountRoutes.js');
 const catTransactionRoutes = require('./routes/catTransactionRoutes.js');
+const accountRoutes = require('./routes/accountRoutes.js');
 
 // Middleware pour traiter les données JSON
 app.use(express.json());
@@ -17,6 +18,8 @@ app.use('/users', userRoutes);
 app.use('/type-account', typeAccountRoutes);
 // Utilisation du routeur pour les types de compte sous la route /category-transactions
 app.use('/category-transactions', catTransactionRoutes);
+// Utilisation du routeur pour les comptes sous la route /accounts
+app.use('/accounts', accountRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
